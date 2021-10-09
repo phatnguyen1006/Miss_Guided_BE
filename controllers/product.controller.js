498b8868fe645aff9454058c17ba125f5d81544d
KhoiLe <Khoilecontact@gmail.com>
2021-10-09 10:59:40 +0700

    9/10 - add isNextPage

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,8 +4,7 @@ module.exports.getAllProducts = async (req, res) => {
     const allProducts = await Products.findAllProducts();
 
     if (allProducts) {
-        const JSONProducts = JSON.stringify(allProducts);
-        return res.status(200).json(JSONProducts);
+        return res.status(200).json({"products": allProducts});
     } else {
         return res.status(400).json({"message": "Failed to get all products"});
     }
@@ -17,21 +16,26 @@ module.exports.getOneProduct = async (req, res) => {
     const findProduct = await Products.findOneProduct({_id: id});
     
     if (findProduct) {
-        const JSONProduct = JSON.stringify(findProduct);
-        res.status(200).json(JSONProduct);
+        res.status(200).json({ "products": findProduct });
     } else {
         res.status(401).json({"message": "No product matches"});
     }
 }
 
 module.exports.getPagination = async (req, res) => {
+    var isNextPage = true;
     const page = req.params.page;
-      
+    
     const products = await Products.findPagination(page);
+    const nextPageProduct = await Products.findPagination(page+1);
+    
+    if (nextPageProduct.length == 0) {
+        isNextPage = false;
+    }
 
     if (!products) return res.status(400).json({"message": "Faild to get pagination products"});
     else {
         // const JSONProducts = JSON.stringify(products);
-        return res.status(200).json({ "products": products });
+        return res.status(200).json({ "products": products, "isNextPage": isNextPage });
     }
 }
