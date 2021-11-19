dc22a7eb47897abdd86eed4ae47055605ad5fba8
P <19521998@gm.uit.edu.vn>
2021-11-19 23:42:03 +0700

    P - Add RemoveFromCart Fn

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,88 +1,99 @@
-const userService = require('../services/user.service');
+const userService = require("../services/user.service");
 
 module.exports.postRegister = async (req, res) => {
-    const UserData = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName, 
-        email: req.body.email, 
-        password: req.body.password,
-        wishlist: req.body.wishlist,
-        cart: req.body.cart,
-        dob: req.body.dob,
-    };
+  const UserData = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    wishlist: req.body.wishlist,
+    cart: req.body.cart,
+    dob: req.body.dob,
+  };
 
-    const isEmailExisted = await userService.loginUser({email: req.body.email});
-    if (isEmailExisted) {
-        res.status(401).json({"message": "Exsisted Email !!"});
-        return;
-    }
+  const isEmailExisted = await userService.loginUser({ email: req.body.email });
+  if (isEmailExisted) {
+    res.status(401).json({ message: "Exsisted Email !!" });
+    return;
+  }
 
-    //Register user
-    const registeredUser = await userService.registerUser(UserData);
+  //Register user
+  const registeredUser = await userService.registerUser(UserData);
 
-    if (registeredUser) {
-        res.status(200).json({user: registeredUser});
-    } else {
-        res.status(400).json({"message": "Fail to register!!!"});
-    }
-}
+  if (registeredUser) {
+    res.status(200).json({ user: registeredUser });
+  } else {
+    res.status(400).json({ message: "Fail to register!!!" });
+  }
+};
 
 module.exports.postLogin = async (req, res) => {
-    var email = req.body.email;
-    var password = req.body.password;
+  var email = req.body.email;
+  var password = req.body.password;
 
-    if (!password) return res.status(401).json({ "message": "Please enterpassword" });
+  if (!password)
+    return res.status(401).json({ message: "Please enterpassword" });
 
-    let data = { email, password };
-    
-    let findUser = await userService.loginUser(data);
-    
-    if (findUser) {
-        // return res.status(200).json({ "message": login });
-        if (findUser == 200) {
-            return res.status(200).json({ user: findUser});
-        } else if (findUser == 401) {
-            return res.status(401).json({ "message": "Wrong password !!!" });
-        }
-    } else {
-        return res.status(401).json({ "message": "Fail to login" });
+  let data = { email, password };
+
+  let findUser = await userService.loginUser(data);
+
+  if (findUser) {
+    // return res.status(200).json({ "message": login });
+    if (findUser == 200) {
+      return res.status(200).json({ user: findUser });
+    } else if (findUser == 401) {
+      return res.status(401).json({ message: "Wrong password !!!" });
     }
-}
+  } else {
+    return res.status(401).json({ message: "Fail to login" });
+  }
+};
 
 module.exports.findUser = async (req, res) => {
-    var email = req.body.email;
+  var email = req.body.email;
 
-    const userFound = await userService.findUser(email);
+  const userFound = await userService.findUser(email);
 
-    if (userFound) {
-        return res.status(200).json({ user: userFound });
-    } else {
-        return res
-      .status(400)
-      .json({ message: "Faild to find user" });
-    }
-}
+  if (userFound) {
+    return res.status(200).json({ user: userFound });
+  } else {
+    return res.status(400).json({ message: "Faild to find user" });
+  }
+};
 
 module.exports.addToCart = async (req, res) => {
-    const { email, newProduct } = req.body;
+  const { email, newProduct } = req.body;
 
-    const onUpdateCart = await userService.updateCart(email, newProduct);
-    
-    if (onUpdateCart) {
-        res.status(200).json({ "message": onUpdateCart });
-    } else {
-        res.status(400).json({ "message": "Add to cart failed !!!" });
-    }
-}
+  const onUpdateCart = await userService.updateCart(email, newProduct);
+
+  if (onUpdateCart) {
+    res.status(200).json({ message: onUpdateCart });
+  } else {
+    res.status(400).json({ message: "Add to cart failed !!!" });
+  }
+};
+
+module.exports.removeFromCart = async (req, res) => {
+  const { email, productId } = req.body;
+
+  const onUpdateCart = await userService.removeFromCart(email, productId);
+
+  if (onUpdateCart) {
+    res.status(200).json({ message: "Remove successfully" });
+  } else {
+    res.status(400).json({ message: "Remove from cart failed !!!" });
+  }
+};
 
 module.exports.addToWishlist = async (req, res) => {
-    const { email, newProduct } = req.body;
+  const { email, newProduct } = req.body;
 
-    const onUpdateWishlist = await userService.updateWishlist(email, newProduct);
-    
-    if (onUpdateWishlist) {
-        res.status(200).json({ "message": onUpdateWishlist });
-    } else {
-        res.status(400).json({ "message": "Add to cart failed !!!" });
-    }
-}
+  const onUpdateWishlist = await userService.updateWishlist(email, newProduct);
+
+  if (onUpdateWishlist) {
+    res.status(200).json({ message: onUpdateWishlist });
+  } else {
+    res.status(400).json({ message: "Add to cart failed !!!" });
+  }
+};
--- services/user.service.js
+++ services/user.service.js
@@ -2,120 +2,140 @@ const Users = require("../models/user.model");
 const { compare } = require("../helper/comparePassword");
 
 async function registerUser(data) {
-    try {
-        const newUser = new Users(data); 
-        
-        const saveUser = await newUser.save();
+  try {
+    const newUser = new Users(data);
 
-        if (saveUser) console.log('Register successful');
-        else console.log('Error in creating new user');
+    const saveUser = await newUser.save();
 
-        return saveUser;
-    } catch (error) {
-        console.log("Error in creating user: ", error.message);
-        throw error;
-    }
+    if (saveUser) console.log("Register successful");
+    else console.log("Error in creating new user");
+
+    return saveUser;
+  } catch (error) {
+    console.log("Error in creating user: ", error.message);
+    throw error;
+  }
 }
 
 async function loginUser(data) {
-    try {
-        const userFound = await Users.findOne({ email: data.email });
+  try {
+    const userFound = await Users.findOne({ email: data.email });
 
-        if (userFound) {
-            return compare(data.password, userFound.password) ? 200 : 401;
-        }
-        else {
-            return null;
-        }
-    } catch(err) {
-        console.log("Error in login user: ", err.message);
+    if (userFound) {
+      return compare(data.password, userFound.password) ? 200 : 401;
+    } else {
+      return null;
     }
+  } catch (err) {
+    console.log("Error in login user: ", err.message);
+  }
 }
 
 async function updateUser(email, data) {
-    const userUpdate = await Users.findOneAndUpdate({
-        email: email,
-    }, data, {
-        new: true
-    }, (error) => {
-        if(!err) console.log("Update user successful!");
-        
-        else console.log("Udate user failed!");
-    });
+  const userUpdate = await Users.findOneAndUpdate(
+    {
+      email: email,
+    },
+    data,
+    {
+      new: true,
+    },
+    (error) => {
+      if (!err) console.log("Update user successful!");
+      else console.log("Udate user failed!");
+    }
+  );
 
-    return userUpdate;
+  return userUpdate;
 }
 
 async function findUser(email) {
-    try {
-        const userFound = await Users.findOne({ email: email });
+  try {
+    const userFound = await Users.findOne({ email: email });
 
-        if (userFound) {
-            return userFound;
-        }
-        else {
-            return null;
-        }
-    } catch(err) {
-        console.log("Error in login user: ", err.message);
+    if (userFound) {
+      return userFound;
+    } else {
+      return null;
     }
+  } catch (err) {
+    console.log("Error in login user: ", err.message);
+  }
 }
 
 async function updateCart(email, newProductId) {
-    // If Cart [] || Cart !- []
-    try {
-        const user = await Users.findOne({ email: email });
+  // If Cart [] || Cart !- []
+  try {
+    const user = await Users.findOne({ email: email });
 
-        user.cart.push(newProductId);
-        await user.save();
+    user.cart.push(newProductId);
+    await user.save();
 
-        return user;
-    } catch (err) {
-        return null;
-    }
+    return user;
+  } catch (err) {
+    return null;
+  }
 }
 
 async function updateWishlist(email, newProductId) {
-    // If Cart [] || Cart !- []
-    try {
-        const user = await Users.findOne({ email: email });
+  // If Cart [] || Cart !- []
+  try {
+    const user = await Users.findOne({ email: email });
 
-        user.wishlist.push(newProductId);
-        await user.save();
+    user.wishlist.push(newProductId);
+    await user.save();
 
-        return user;
-    } catch (err) {
-        return null;
+    return user;
+  } catch (err) {
+    return null;
+  }
+}
+
+async function removeFromCart(email, productId) {
+  try {
+    const user = await Users.findOne({ email: email }).lean();
+
+    user.cart = user.cart.filter((c) => c != productId);
+
+    var result = await user.save();
+
+    if (result) {
+      return true;
+    } else {
+      return null;
     }
+  } catch (err) {
+    return null;
+  }
 }
 
 async function removeFromWishList(email, productId) {
-    try {
-        const user = await Users.findOne({ email: email });
+  try {
+    const user = await Users.findOne({ email: email });
 
-        removeElement(user.wishlist, productId);
-        await user.save();
+    removeElement(user.wishlist, productId);
+    await user.save();
 
-        return user;
-    } catch (err) {
-        return null;
-    }
+    return user;
+  } catch (err) {
+    return null;
+  }
 }
 
 function removeElement(array, elem) {
-    var index = array.indexOf(elem);
-    if (index > -1) {
-        array.splice(index, 1);
-    }
+  var index = array.indexOf(elem);
+  if (index > -1) {
+    array.splice(index, 1);
+  }
 }
 
-
 module.exports = {
-    registerUser,
-    loginUser,
-    updateUser,
-    updateCart,
-    updateWishlist,
-    removeFromWishList,
-    findUser,
+  registerUser,
+  loginUser,
+  updateUser,
+  updateCart,
+  updateWishlist,
+  removeFromWishList,
+  findUser,
+  removeFromCart,
 };
