3329b32b6129b1d4fcf2f854f9305185d0873293
KhoiLe <Khoilecontact@gmail.com>
2021-11-19 11:01:07 +0700

    add get user info controller

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/user.controller');
 
 router.post('/addToCart', controller.addToCart);
 router.post('/addToWishlist', controller.addToWishlist);
+router.post('/getInfo', controller.findUser)
 
 module.exports = router;
